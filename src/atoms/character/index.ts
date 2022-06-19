72ef57e23ec2da165727b40f843e9d029c4079cb
Opion <kindled0kindred@icloud.com>
2022-06-19 15:52:42 +0900

    feat: character-origin

--- src/atoms/character/index.ts
+++ src/atoms/character/index.ts
@@ -3,18 +3,24 @@
  */
 
 export const CHARACTER_ORIGIN = {
-  PROPHET: {
-    name: 'prophet',
-    icon: '',
-    description: 'prophet_description',
-  }
-}
+  prophet: {
+    name: "prophet",
+    icon: "",
+    description: "prophet_description",
+  },
+  assassin: {
+    name: "assassin",
+    icon: "",
+    description: "assassin_description",
+  },
+} as const;
 
 export type CharacterOrigin = typeof CHARACTER_ORIGIN[keyof typeof CHARACTER_ORIGIN];
+export type CharacterOriginName = keyof typeof CHARACTER_ORIGIN;
 
 export type Character = {
   name: string;
   origin: CharacterOrigin; // 出自
   deck: string[]; // スキルのID配列
   blessing: string[]; // 祝福のID配列
-}
+};

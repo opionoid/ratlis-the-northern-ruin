24afa1b462953eb667c665dff502108bf27fe2f0
Opion <kindled0kindred@icloud.com>
2022-06-19 15:51:58 +0900

    feat: trailingSlash

--- next.config.js
+++ next.config.js
@@ -1,16 +1,17 @@
 /** @type {import('next').NextConfig} */
 
 // publicの静的ファイルのimportパス解消
-const pathPrefix = process.env.NEXT_PUBLIC_PATH_PREFIX || '';
+const pathPrefix = process.env.NEXT_PUBLIC_PATH_PREFIX || "";
 
 // 多言語
-const { i18n } = require('./next-i18next.config');
+const { i18n } = require("./next-i18next.config");
 
 const nextConfig = {
   reactStrictMode: true,
   assetPrefix: pathPrefix,
   basePath: pathPrefix,
-  i18n
-}
+  trailingSlash: true,
+  i18n,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
